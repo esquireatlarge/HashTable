ce9ac0f7af319aa770ac6ced1d416d177aa33b35
esquireatlarge <mattsguerri@gmail.com>
2015-08-24 16:59:22 -0700

    Added item and linked list

---
+++ HashTable/Item.h
@@ -0,0 +1,22 @@
+//Esquire At Large
+//Represents an item storable within a linked list.
+//Taken from Linked List repo.
+
+template <class T>
+class Item
+{
+public:
+
+    Item(T data)
+        : m_data(data), m_next(nullptr)
+    {
+    }
+
+    inline T GetData(){ return m_data };
+    inline Item<T>* GetNext(){ return m_next; }
+    void SetNext(Item<T>* n){ m_next = n; }
+
+private:
+    T        m_data;
+    Item<T>* m_next;
+};
